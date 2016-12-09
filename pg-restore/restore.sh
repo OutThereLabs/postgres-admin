8adb086b9a6aa3d174b4f16028a6eb089a412c3f
Julian Tescher <jatescher@gmail.com>
2016-12-09 14:49:09 -0800

    Add pg restore image

--- pg-restore/restore.sh
+++ pg-restore/restore.sh
@@ -39,10 +39,21 @@ fi
 export PGPASSWORD=$POSTGRESQL_PASSWORD
 POSTGRESQL_CONNECTION_OPTS="-h $POSTGRESQL_HOST -p $POSTGRESQL_PORT -U $POSTGRESQL_USER $POSTGRESQL_EXTRA_OPTS"
 
-echo "Creating dump of ${POSTGRESQL_DATABASE} database from ${POSTGRESQL_HOST}..."
-pg_dump $POSTGRESQL_CONNECTION_OPTS $POSTGRESQL_DATABASE | gzip > /var/backups/dump.sql.gz
+if [ "${BACKUP_FILE_NAME}" = "latest" ]; then
+  BACKUP_FILE_NAME=$(aws s3 ls s3://$S3_BUCKET/$S3_PREFIX | sort | tail -n 1 | awk '{ print $4 }')
+fi
+echo "Getting db backup ${BACKUP_FILE_NAME} from S3"
 
-echo "Uploading dump to $S3_BUCKET"
-cat /var/backups/dump.sql.gz | aws s3 cp - s3://$S3_BUCKET/$S3_PATH$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz || exit 2
+aws s3 cp s3://$S3_BUCKET/$S3_PREFIX${BACKUP_FILE_NAME} /var/restore/dump.sql.gz
+gzip -d /var/ressstore/dump.sql.gz
 
-echo "SQL backup uploaded successfully"
+if [ "${DROP_PUBLIC}" == "yes" ]; then
+	echo "Recreating the public schema"
+	psql $POSTGRES_HOST_OPTS -d $POSTGRES_DATABASE -c "drop schema public cascade; create schema public;"
+fi
+
+echo "Restoring ${BACKUP_FILE_NAME}"
+
+psql $POSTGRESQL_CONNECTION_OPTS -d $POSTGRES_DATABASE < /var/restore/dump.sql
+
+echo "Restore complete"
